1bc9bdd16b7bf07987a36adacb2f6766be610bf4
Sunderfal <sergiovdg2016@gmail.com>
2025-06-10 12:21:26 +0200

    Added tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", 
+  ],
+  safelist: [
+    "opacity-0",
+    "opacity-100",
+    "translate-y-0",
+    "translate-y-20",
+  ],
+}
